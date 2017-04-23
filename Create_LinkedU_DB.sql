d0155bb6b33d91fff2994ae35b25ac58dd003945
Perry <pdkaufm@ilstu.edu>
2017-04-23 14:31:41 -0500

    Updated table creation file
    Now fills tables with 4 student accounts. Student Profile table added as
    well.

--- Create_LinkedU_DB.sql
+++ Create_LinkedU_DB.sql
@@ -1,12 +1,25 @@
 --drop schema Project353 restrict;
-DROP TABLE LINKEDU.ApplyInfo;
-drop table LinkedU.LoginInfo;
-drop table LinkedU.Users;
+--drop table LinkedU.LoginInfo;
+--drop table LinkedU.Users;
+--drop table LINKEDU.ApplyInfo;
+--drop table LinkedU.StudentProfile;
 create table LinkedU.LoginInfo (
   UserID                    VARCHAR(25) NOT NULL,
   Password                  VARCHAR(50) NOT NULL
 ); 
 
+--Student Accounts
+insert into LinkedU.LoginInfo (UserID, Password) values
+('pdkaufm', '40bf696d25dd56ed44c864e05f75d33a4cface91');
+insert into LinkedU.LoginInfo (UserID, Password) values
+('ksuth1', '40bf696d25dd56ed44c864e05f75d33a4cface91');
+insert into LinkedU.LoginInfo (UserID, Password) values
+('stiwar1', '40bf696d25dd56ed44c864e05f75d33a4cface91');
+insert into LinkedU.LoginInfo (UserID, Password) values
+('asriren', '40bf696d25dd56ed44c864e05f75d33a4cface91');
+
+--University Accounts
+--to be added
 
 create table LinkedU.Users (
   UserID                    VARCHAR(25) NOT NULL,
@@ -18,6 +31,18 @@ create table LinkedU.Users (
   SecurityAnswer            VARCHAR(50) NOT NULL
 );
 
+--Student Accounts
+insert into LinkedU.Users(UserID, FirstName, LastName, Email, AccountType, SecurityQuestion, SecurityAnswer) values
+('pdkaufm','Perry','Kaufman','pdkaufm@ilstu.edu','Student','Q?','A');
+insert into LinkedU.Users(UserID, FirstName, LastName, Email, AccountType, SecurityQuestion, SecurityAnswer) values
+('kssuth1','Keegan','Sutherland','kssuth1@ilstu.edu','Student','Q?','A');
+insert into LinkedU.Users(UserID, FirstName, LastName, Email, AccountType, SecurityQuestion, SecurityAnswer) values
+('stiwar1','Shivangi','Tiwari','stiwar1@ilstu.edu','Student','Q?','A');
+insert into LinkedU.Users(UserID, FirstName, LastName, Email, AccountType, SecurityQuestion, SecurityAnswer) values
+('asriren','Anusha','Srirenganathan Malarvizhi','asriren@ilstu.edu','Student','Q?','A');
+
+--University Accounts
+--to be added
 
 CREATE TABLE LINKEDU.ApplyInfo(
 University varchar(50) NOT NULL,
@@ -38,3 +63,10 @@ Score varchar(35) NOT NULL,
 High_school varchar(100) NOT NULL, 
 High_School_Address varchar(50) NOT NULL,
 High_School_Country varchar(50) NOT NULL);
+
+create table LinkedU.StudentProfile(
+ACT				float(32) not null,
+SAT				float(32) not null,
+PSAT_NMSQT		float(32) not null,
+Universities	varchar(200) not null,
+Majors			varchar(200) not null);
